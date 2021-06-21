7951a72ab91f358b24d3eb0d07812308fb2275b9
Ritvik Shukla <72812470+includestdlibrary@users.noreply.github.com>
2021-06-21 20:31:14 +0530

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+
+        for (int j = 1; j <= 5; j++)
+        {
+            if (j >= i && j <= 5)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
