33a5d8f138e3be79f3ff09ef7de20e95e2159ba7
Ritvik Shukla <72812470+includestdlibrary@users.noreply.github.com>
2021-06-22 15:34:32 +0530

    Add files via upload

---
+++ q22.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 7; i++)
+    {
+        for (int j = 1; j <= 7; j++)
+        {
+            if ((i == 1 || j == 1 || i == 7 || j == 7) || ((i == 3 || j == 3 || i == 5 || j == 5) && j >= 3 && j <= 5 && i >= 3 && i <= 5))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
